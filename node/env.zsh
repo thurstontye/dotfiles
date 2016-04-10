e7649d8a6fdce81b6848e9a3edefd97c6710cb6e
Limess <charliebriggs@gmail.com>
2016-04-10 18:39:35 +0100

    Further refinements

--- node/env.zsh
+++ node/env.zsh
@@ -1,2 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
 export PATH="./node_modules/.bin:$PATH"
+
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
--- node/install.sh
+++ node/install.sh
@@ -1,2 +1,12 @@
-### NVM
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+### NVM (https://github.com/creationix/nvm)
+if [ ! -d "$NVM_DIR" ];
+then
+    echo "installing nvm"
+    git clone https://github.com/creationix/nvm.git ~/.nvm > /dev/null && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags` > /dev/null
+else
+    echo "attempting to update nvm"
+    cd "$NVM_DIR" && git pull origin master > /dev/null && git checkout `git describe --abbrev=0 --tags` >/dev/null
+fi;
+
+cd -
+
