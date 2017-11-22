408284dd83294d0c740d8fdf694247bdee766d18
Charlie Briggs <charlie.briggs@ft.com>
2017-11-22 09:43:03 +0000

    Add go/kubes configuration

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,7 @@
 export GOPATH=$PROJECTS/go
 export PATH="$GOPATH/bin:$PATH"
+
+if [ -x "$(command -v brew)" ];then
+  GOVERSION=$(brew list go | head -n 1 | cut -d '/' -f 6)
+  export GOROOT=$(brew --prefix)/Cellar/go/$GOVERSION/libexec
+fi
