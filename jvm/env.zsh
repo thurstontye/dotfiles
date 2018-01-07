fe0ff3cece830fb3dfe3f60544cfb3da15898ade
Charlie Briggs <charlie.briggs@ft.com>
2018-01-07 20:44:36 +0000

    Update java version

--- jvm/env.zsh
+++ jvm/env.zsh
@@ -1,3 +1,3 @@
 export SCALA_HOME="/usr/local/opt/scala/idea"
 export JAVA_OPTS="${JAVA_OPTS} -Djava.net.useSystemProxies=true"
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home"
+export JAVA_HOME="$(/usr/libexec/java_home)"
