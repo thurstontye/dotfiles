4b40ecea75d269e6f48f31bae375b4258abc82e8
Charlie Briggs <charlie.briggs@ft.com>
2018-04-25 11:25:01 +0100

    Optional JAVA_HOME setting

--- jvm/env.zsh
+++ jvm/env.zsh
@@ -1,3 +1,5 @@
 export SCALA_HOME="/usr/local/opt/scala/idea"
 export JAVA_OPTS="${JAVA_OPTS} -Djava.net.useSystemProxies=true"
-export JAVA_HOME="$(/usr/libexec/java_home)"
+if [ -f /usr/libexec/java_home ]; then
+    export JAVA_HOME="$(/usr/libexec/java_home)"
+fi
