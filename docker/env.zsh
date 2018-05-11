ef74e52d91ea3d6039db8cdd994d4132f77a56d8
Charlie Briggs <charlie.briggs@ft.com>
2018-05-11 14:38:26 +0100

    Add mounts for windows

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,3 +1,13 @@
-if grep -q Microsoft /proc/version; then   
+if [ -e /proc/version ] && grep -q Microsoft /proc/version; then
+  # Setup docker for WSL https://nickjanetakis.com/blog/setting-up-docker-for-windows-and-wsl-to-work-flawlessly
   export DOCKER_HOST="tcp://0.0.0.0:2375"
+  if [ -d /c ]; then
+    sudo mount --bind /mnt/c /c
+  fi
+  if [ -d /d ]; then
+    sudo mount --bind /mnt/d /d
+  fi
+  if [ -d /e ]; then
+    sudo mount --bind /mnt/e /e
+  fi
 fi
