a1817cb4c98cc41c940f41e1b3a4d584da965930
Charlie Briggs <charlie.briggs@ft.com>
2019-04-04 11:45:11 +0100

    Add node alias

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,3 +1,15 @@
-alias default_npm="npmrc default" # https://github.com/nolanlawson/local-npm-launch-agent
+# prevent bash completion from correcting serverless
 alias serverless="nocorrect serverless"
 alias npmu="npx npm-check -u"
+# prevent spotlight/alfred indexing node_modules
+# https://twitter.com/Herschel_R/status/1113095592559984641
+function npm_install {
+    mkdir -p node_modules
+    touch ./node_modules/.metadata_never_index
+    if [ -f yarn.lock ]; then
+        yarn install $@
+    else
+        npm install $@
+    fi
+}
+alias ni=npm_install
