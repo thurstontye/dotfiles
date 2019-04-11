740ef311ab9182bff246e57043cb35eb5c2ee4c4
Charlie Briggs <charlie.briggs@ft.com>
2019-04-11 09:46:34 +0100

    Remove cask versions from osx brewfile, remove go version from path

--- go/path.zsh
+++ go/path.zsh
@@ -1,5 +1,4 @@
 export GOPATH=$PROJECTS/go
-export PATH="/usr/lib/go-1.10/bin:$GOPATH/bin:$PATH"
 
 if [ -x "$(command -v brew)" ];then
   GOVERSION=$(brew list go 2>/dev/null | head -n 1 | cut -d '/' -f 6)
