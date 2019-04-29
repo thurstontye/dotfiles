f4b3a1cbc660eef20e133d33c3c2ae0112a01d8c
Charlie Briggs <charlie.briggs@ft.com>
2019-04-29 10:34:38 +0100

    Add nvm zsh plugin

--- node/env.zsh
+++ node/env.zsh
@@ -5,4 +5,11 @@ export NODE_REPL_HISTORY_SIZE='32768';
 # Use sloppy mode by default, matching web browsers.
 export NODE_REPL_MODE='sloppy';
 
-# NVM is sourced by prezto (https://github.com/sorin-ionescu/prezto/tree/master/modules/node)
+
+# lazy load nvm
+# https://github.com/lukechilds/zsh-nvm#lazy-loading
+export NVM_LAZY_LOAD=true
+# autoload nvm
+# https://github.com/lukechilds/zsh-nvm#auto-use
+export NVM_AUTO_USE=true
+
