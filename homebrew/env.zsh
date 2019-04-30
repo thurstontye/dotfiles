707d0542e508fa406393c1c144f729cff148be9b
Charlie Briggs <charliebriggs@gmail.com>
2019-04-30 23:58:05 +0100

    Update env.zsh

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,7 +1,8 @@
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-eval $($(brew --prefix)/bin/brew shellenv)
 
-export PATH="$($(brew --prefix)/bin/brew shellenv):$(brew --prefix)/sbin:$PATH"
+BREW_PREFIX=$(brew --prefix)
+eval $($BREW_PREFIX/bin/brew shellenv)
+
 export MANPATH="$BREWPATH//share/man:$MANPATH"
 export INFOPATH="$BREWPATH/share/info:$INFOPATH"
