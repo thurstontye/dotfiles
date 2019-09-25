2d22012f0f4d4f29f687d81803acfc2ef6119517
Charlie Briggs <charlie.briggs@signal-ai.com>
2019-09-25 10:26:59 +0100

    Add pyenv and poetry

--- python/env.zsh
+++ python/env.zsh
@@ -1,4 +1,7 @@
 export PIPENV_VENV_IN_PROJECT=true
 export PATH=$PATH:$(python3 -c "import site; print(site.USER_BASE)")/bin/
 export PATH=$PATH:$(python -c "import site; print(site.USER_BASE)")/bin/
-# eval "$(pyenv virtualenv-init -)"
+export PATH=$HOME/.poetry/bin:$PATH
+if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init -)"
+fi
