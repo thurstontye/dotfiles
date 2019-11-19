263e37a903c2aee9c9e83488f570d5d4bff997d9
Charlie Briggs <charlie.briggs@signal-ai.com>
2019-11-19 09:23:27 +0000

    Tweaks and additions

--- python/env.zsh
+++ python/env.zsh
@@ -1,4 +1,6 @@
 export PIPENV_VENV_IN_PROJECT=true
+export POETRY_VIRTUALENVS_IN_PROJECT=true
+
 export PATH=$PATH:$(python3 -c "import site; print(site.USER_BASE)")/bin/
 export PATH=$PATH:$(python -c "import site; print(site.USER_BASE)")/bin/
 export PATH=$HOME/.poetry/bin:$PATH
