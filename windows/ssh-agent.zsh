d6f38656ff8ccf62cafb3a4b3f9076af6c0baecc
Charlie Briggs <charlie.briggs@signal-ai.com>
2019-12-10 17:25:34 +0000

    Quote variable

--- windows/ssh-agent.zsh
+++ windows/ssh-agent.zsh
@@ -1,5 +1,5 @@
 if [ -z "$SSH_TTY" ]; then
-	if [ ${IS_WSL} -eq 1 ]; then
+	if [ "${IS_WSL}" -eq 1 ]; then
 	    # http://manpages.ubuntu.com/manpages/xenial/man1/keychain.1.html
             eval `keychain -q --eval id_rsa`
 	fi
