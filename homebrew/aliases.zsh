f2d696e3c411e8191c5863fef6c46836bff7ef92
Charlie Briggs <charliebriggs@gmail.com>
2020-06-09 00:03:42 +0100

    Remove defunkt brew command

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,5 +1,4 @@
-alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
-alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-rdoc --no-ri'
+alias brew_update="brew -v update; brew upgrade --force-bottle; brew cleanup; brew cask cleanup; brew prune; brew doctor;"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # brew cask is Mac OS only
