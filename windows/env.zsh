2599e60f57509237b3de3dc8c0f236468aad7e36
Charlie Briggs <charliebriggs@gmail.com>
2020-06-14 09:47:23 +0100

    Working browser command on WSL

--- windows/env.zsh
+++ windows/env.zsh
@@ -1,1 +1,7 @@
 export IS_WSL=$(uname -a | grep -i -c "microsoft")
+
+if [[ "${IS_WSL}" -eq 1 ]]; then
+    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
+
+    alias open="$BROWSER $1"
+fi
