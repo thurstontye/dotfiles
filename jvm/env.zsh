73d794d45af6f2e4d9758b8710a32c328ffe793e
Charlie Briggs <charlie.briggs@signal-ai.com>
2020-06-20 23:12:44 +0100

    Fix jenv init script
    Looks the same but one wasn't evaling correctly

--- jvm/env.zsh
+++ jvm/env.zsh
@@ -2,7 +2,7 @@ export PATH=”$HOME/.jenv/bin:$PATH”
 export SCALA_HOME="/usr/local/opt/scala/idea"
 export JAVA_OPTS="${JAVA_OPTS} -Djava.net.useSystemProxies=true"
 
-eval “$(jenv init -)”
+eval "$(jenv init -)"
 
 if [ -f /usr/libexec/java_home ]; then
 	JENV_VERSION_NAME=$(jenv version-name)
